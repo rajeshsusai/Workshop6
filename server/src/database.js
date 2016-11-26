c6360f1dcc616de072d8ce476089c65b98649d3a
Rajesh Susai <rajeshsusai14@gmail.com>
2016-11-26 18:51:52 -0500

    database

--- server/src/database.js
+++ server/src/database.js
@@ -1,4 +1,5 @@
 // Data goes here.
+// I read the section on migrating data to mock database on server.
 var initialData = {
   // The "user" collection. Contains all of the users in our Facebook system.
   "users": {
