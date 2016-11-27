f745e3547ce2fac9c0fff99dae65e2bfd2c27e52
Rajesh Susai <rajeshsusai14@gmail.com>
2016-11-26 20:45:52 -0500

    failbook

--- client/app/server.js
+++ client/app/server.js
@@ -10,6 +10,64 @@ function emulateServerReturn(data, cb) {
   }, 4);
 }
 
+var token = 'eyJpZCI6NH0=';
+
+function sendXHR(verb, resource, body, cb) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(verb, resource);
+  xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+  //The below comment tells ESLint that FaceBookError is a global.
+  //Otherwise, ESLint would complain about it! (See what happens in Atom if
+  //you remove the comment...)
+  /* global FacebookError */
+
+  // Response received from server. It could be a failure though!
+  xhr.addEventListener('load', function() {
+    var statusCode = xhr.status;
+    var statusText = xhr.statusText;
+    if(statusCode >= 200 && statusCode < 300) {
+      //success
+      cb(xhr);
+    }
+    else {
+      //Client or server error.
+      var responseText = xhr.responseText;
+      FacebookError('Could not ' + verb + " " + resource + ": Received " +
+                      statusCode + " " + statusText + ": " + responseText);
+    }
+  });
+  xhr.timeout = 10000;
+
+  //Network failure: Could not connect to server.
+  xhr.addEventListener('error', function() {
+    FacebookError('Could not ' + verb + " " + resource +
+                  ": Could not connect to the server.");
+  });
+
+  //Network failure: request took too long to complete.
+  xhr.addEventListener('timeout', function() {
+    FacebookError('Could not ' + verb + " " + resource +
+                  ": Request timed out.");
+  });
+
+  switch(typeof(body)) {
+    case 'undefined':
+      // No body to send.
+      xhr.send();
+      break;
+    case 'string':
+      // Tell the server we are sending text.
+      xhr.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+      xhr.send(body);
+      break;
+    case 'object':
+      // Tell the server we are sending JSON.
+      xhr.send(JSON.stringify(body));
+      break;
+    default:
+      throw new Error('Uknown body type: ' + typeof(body));
+  }
+}
 /**
  * Resolves a feed item. Internal to the server, since it's synchronous.
  */
@@ -31,13 +89,11 @@ function getFeedItemSync(feedItemId) {
  * Emulates a REST call to get the feed data for a particular user.
  */
 export function getFeedData(user, cb) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/user/4/feed');
-  xhr.setRequestHeader('Authorization', 'Bearer eyJpZCI6NH0=');
-  xhr.addEventListener('load', function() {
+  // We don't need to send a body, so pass in 'undefined' for the body.
+  sendXHR('GET', '/user/4/feed', undefined, (xhr) => {
+    // Call the callback with the data.
     cb(JSON.parse(xhr.responseText));
   });
-  xhr.send();
 }
 
 /**
